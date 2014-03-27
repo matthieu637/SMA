e402507eb7ae51631be569a303dc5f5ea6de03fa
Matthieu Zimmer <contact@matthieu-zimmer.net>
2014-03-27 13:05:25 +0100

    zone de vue des drones

--- project/src/java/modele/CielModel.java
+++ project/src/java/modele/CielModel.java
@@ -3,7 +3,9 @@ package modele;
 import jason.environment.grid.Location;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import modele.entite.Convoi;
 import modele.entite.Drone;
@@ -33,17 +35,24 @@ public class CielModel extends Grille {
 
 	private AllPercepts interpreteur;
 
+	private Map<Location, List<Integer>> zones;
+
 	protected CielModel(int nbAgent, AllPercepts interpreteur, List<EntiteComportement> agentsSupplementaires, Convoi convoi) {
 		super(Variables.TAILLE_CARTE_X, Variables.TAILLE_CARTE_Y, nbAgent, interpreteur, false);
 		this.agentsSupplementaires = agentsSupplementaires;
 		this.convoi = convoi;
-		
+
 		drones = new ArrayList<Drone>(nbAgent);
-		int i = 0;
-		for (; i < nbAgent; i++)
+
+		for (int i = 0; i < nbAgent; i++)
 			drones.add(new Drone(i + 1, true, getAgPos(i), Variables.CAPACITE_FUEL_DRONE, Variables.CHAMP_VISION_DRONE_BASSE_ALTITUDE,
 					Variables.CHAMP_VISION_DRONE_HAUTE_ALTITUDE));
 		this.interpreteur = interpreteur;
+
+		zones = new HashMap<Location, List<Integer>>();
+		for (int i = 0; i < Variables.TAILLE_CARTE_X; i++)
+			for (int j = 0; j < Variables.TAILLE_CARTE_Y; j++)
+				zones.put(new Location(i, j), new ArrayList<Integer>(nbAgent));
 
 		this.majPercepts();
 	}
@@ -60,18 +69,57 @@ public class CielModel extends Grille {
 	public Couple<Location, Boolean> deplacerCollision(int agent, int direction) {
 
 		Couple<Location, Boolean> c = super.deplacerCollision(agent - 1, direction);
-		
+
 		if (c.second) {
-			boolean outOfFuel = getDrone(agent).deplacer(c.first);
-			getDrone(agent).majPercepts(interpreteur, agentsSupplementaires, convoi);
-			
+			Drone d = getDrone(agent);
+			boolean outOfFuel = d.deplacer(c.first);
+			d.majPercepts(interpreteur, agentsSupplementaires, convoi);
+
 			if (outOfFuel) {
 				interpreteur.killDrone(this.getAgAtPos(c.first));
 				this.remove(GridWorldModelP.AGENT, c.first);
 			}
-		}
 
+			updateZone(d);
+			// view.update();
+		}
 		return c;
+	}
+
+//	private Object lock = new Object();
+
+	private void updateZone(Drone d) {
+//		synchronized (lock) {
+
+			int champ = d.isHaute_altitude() ? d.getChamp_vision_haute_altitude() : d.getChamp_vision_basse_altitude();
+
+			int xstart = d.getPos().x - champ - 3;
+			int xfin = d.getPos().x + champ + 3;
+
+			int ystart = d.getPos().y - champ - 3;
+			int yfin = d.getPos().y + champ + 3;
+
+			for (int i = xstart; i <= xfin; i++)
+				for (int j = ystart; j <= yfin; j++) {
+					Location candidat = new Location(i, j);
+					if (inGrid(i, j)) {
+						List<Integer> actives = zones.get(candidat);
+						if (d.getPos().distanceEuclidean(candidat) < champ) {
+							if (!actives.contains(d.getId()))
+								actives.add(d.getId());
+						} else {
+							if (actives.contains(d.getId()))
+								actives.remove((Integer) d.getId());
+						}
+						view.update(i, j);
+					}
+				}
+//		}
+	}
+
+	public Integer getZone(int x, int y) {
+		Location cle = new Location(x, y);
+		return zones.get(cle).size();
 	}
 
 	public Drone getDrone(int id) {
@@ -79,9 +127,41 @@ public class CielModel extends Grille {
 	}
 
 	public boolean droneTermine() {
-		for(Drone d : drones)
-			if(!d.isAuSol())
+		for (Drone d : drones)
+			if (!d.isAuSol())
 				return false;
 		return true;
 	}
+
+	public List<Drone> getDrones() {
+		return drones;
+	}
+}
+
+class ColorPoint {
+	private Location l;
+	private boolean c;
+
+	public ColorPoint(Location l, boolean c) {
+		super();
+		this.l = l;
+		this.c = c;
+	}
+
+	public Location getL() {
+		return l;
+	}
+
+	public void setL(Location l) {
+		this.l = l;
+	}
+
+	public boolean getC() {
+		return c;
+	}
+
+	public void setC(boolean c) {
+		this.c = c;
+	}
+
 }
--- project/src/java/vue/CielView.java
+++ project/src/java/vue/CielView.java
@@ -1,10 +1,20 @@
 package vue;
 
+import jason.environment.grid.Location;
+
+import java.awt.AlphaComposite;
 import java.awt.Color;
+import java.awt.Composite;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.util.Hashtable;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 
 import modele.CielModel;
 import modele.Grille;
+import modele.entite.Drone;
 import ext.GridWorldModelP;
 import ext.GridWorldViewPanel;
 
@@ -17,7 +27,9 @@ public class CielView extends GridWorldViewPanel {
 
 	private static final long serialVersionUID = 1L;
 	private static final Color azurin = new Color(169, 234, 254);
+
 	private static final Color lawn_green = new Color(124, 252, 0);
+
 	private static final Color dark_green = new Color(0, 100, 0);
 
 	public CielView(GridWorldModelP model, int windowSize) {
@@ -32,11 +44,25 @@ public class CielView extends GridWorldViewPanel {
 		g.setColor(azurin);
 		g.fillRect(x * cellSizeW + 1, y * cellSizeH + 1, cellSizeW - 1, cellSizeH - 1);
 		g.setColor(c);
-		
-		if(x < 3 && y == 0){
+
+		if (x < 3 && y == 0) {
 			g.setColor(Color.white);
 			super.drawStringOffset(g, x, y, defaultFont, "P");
 			g.setColor(c);
+		}
+
+		int color = getModele().getZone(x, y);
+		if (color > 0) {
+			float alpha = 0.10f;
+			int type = AlphaComposite.SRC_OVER;
+			AlphaComposite composite = AlphaComposite.getInstance(type, alpha);
+			Graphics2D g2d = (Graphics2D) g;
+			Composite originalComposite = g2d.getComposite();
+			g2d.setComposite(composite);
+			g.setColor(lawn_green);
+			g.fillRect(x * cellSizeW + 1, y * cellSizeH + 1, cellSizeW - 1, cellSizeH - 1);
+
+			g2d.setComposite(originalComposite);
 		}
 	}
 
@@ -44,10 +70,13 @@ public class CielView extends GridWorldViewPanel {
 	public void drawAgent(Graphics g, int x, int y, Color c, int id) {
 		super.drawEmpty(g, x, y);
 
-		if (getModele().getDrone(id + 1).isHaute_altitude())
+		Drone d = getModele().getDrone(id + 1);
+
+		if (d.isHaute_altitude())
 			g.setColor(dark_green);
 		else
 			g.setColor(lawn_green);
+
 		g.fillOval(x * cellSizeW + 2, y * cellSizeH + 2, cellSizeW - 4, cellSizeH - 4);
 	}
 
@@ -67,4 +96,5 @@ public class CielView extends GridWorldViewPanel {
 	public CielModel getModele() {
 		return (CielModel) getModel();
 	}
+
 }
